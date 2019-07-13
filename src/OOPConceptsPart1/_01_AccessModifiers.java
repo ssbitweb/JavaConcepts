c9b7e4e41158f8b87ffa779f0a4dc9e3a220258a
Sachin Bhagat <sachinbhagat0307@gmail.com>
2019-07-13 10:01:28 +0530

    added access modifiers

--- src/OOPConceptsPart1/_01_AccessModifiers.java
+++ src/OOPConceptsPart1/_01_AccessModifiers.java
@@ -6,24 +6,20 @@ package OOPConceptsPart1;
 
 // There are 4 type of access modifiers. 
 // private is highly restrictive & public is least restrictive.
-//
-// access M. |	Class  |  Package |	subClass  |	subClass   |   world
-//			 |		   |		  |(same Pkg) |(diff Pkg)  |
-//------------------------------------------------------------------
-// 1-public		  Y			Y		   Y		   Y		   Y
-// 2-protected	  Y			Y		   Y		   Y		   N
-// 3-default	  Y			Y		   Y		   N		   N
-// 4-private      Y         N          N           N           N
-//-------------------------------------------------------------------
-
-
+//---------------------------------------------------------------------------------------------
+// access modifiers     Class    Package    sbuClass(same pkg)    subClass(diff pkg)    World
+// 1--public              Y        Y               Y                   Y                  Y
+// 2--protected           Y        Y               Y                   Y                  N
+// 3--default             Y        Y               Y                   N                  N
+// 4--private             Y        N               N                   N                  N
+//---------------------------------------------------------------------------------------------
 
 public class _01_AccessModifiers {
 	
-	private int i =10;	 // private
+	private int i =10; // private
 	protected int j =20; // protected
-	int k =30;			 // No access modifiers(Default)
-	public int l=40;     // public
+	int k =30; // No access modifiers(Default)
+	public int l=40; // public
 
 	public static void main(String[] args) {
 
